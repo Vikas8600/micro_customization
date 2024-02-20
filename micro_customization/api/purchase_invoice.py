671e77abd195398ceba83c1ee9b5ecb6f41af1c1
Vikas8600 <vikas.moin777@gmail.com>
2024-02-20 14:41:05 +0530

    feat: property setter changes

--- micro_customization/api/purchase_invoice.py
+++ micro_customization/api/purchase_invoice.py
@@ -1,25 +1,6 @@
 import frappe
 from frappe.utils import now_datetime, add_days, formatdate
 
-_status_after_pr = {
-    "7 days after PR is created": 7,
-    "15 days after PR is created": 15,
-    "30 days after PR is created": 30,
-    "30-45 days after PR is created": 30,
-    "45 days after PR is created": 45,
-    "60 days after PR is created": 60,
-    "100 days after PR is created": 100
-}
-
-_status_after_invoice = {
-    "7 days after invoice creation": 7,
-    "45 days after invoice creation": 45,
-    "60 days after invoice creation": 60,
-    "90 days after invoice creation": 90,
-    "135 days after invoice creation": 135
-}
-
-
 @frappe.whitelist()
 def get_due_date_based_on_condition(pr):
     purchase_receipt = frappe.get_doc("Purchase Receipt", pr)
@@ -31,14 +12,13 @@ def get_due_date_based_on_condition(pr):
     
     if payment_term:
         due_date_based_on = frappe.get_value("Payment Term", payment_term, "due_date_based_on")
+        days_to_add = frappe.get_value("Payment Term", payment_term, "credit_days")
         
-        if due_date_based_on in _status_after_invoice:
-            days_to_add = _status_after_invoice[due_date_based_on]
+        if due_date_based_on == "After invoice creation":
             today_datetime = now_datetime()
             new_date = add_days(today_datetime, days_to_add)
             return {"due_date":frappe.utils.formatdate(new_date.date(), "yyyy-mm-dd")}
-        elif due_date_based_on in _status_after_pr:
-            days_to_add = _status_after_pr[due_date_based_on]
+        elif due_date_based_on == "After PR is created":
             today_datetime = purchase_receipt.posting_date
         else:
             # Handle the case when due_date_based_on is not in either dictionary
